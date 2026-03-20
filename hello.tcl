2c020885fbc7dd02ade6a13b43d83cbb5ae6f0ff
apolodelima@gmail.com <eduarda.froes.santos@ee.ufcg.edu.br>
2026-03-20 19:55:32 -0300

    hello.tcl adicionado

---
+++ hello.tcl
@@ -0,0 +1,7 @@
+puts "Olá, Mundo - Entre aspas"; # Este é um comentário após o comando.
+# Este é um comentário no início de uma linha
+puts {Olá, Mundo - Entre chaves}
+puts "Esta é a linha 1"; puts "esta é a linha 2"
+puts "Olá, Mundo; - Com um ponto e vírgula dentro das aspas"
+# Palavras não precisam ser colocadas entre aspas, a menos que contenham e espaços;
+puts OláMundo

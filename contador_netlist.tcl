82fa5d4be94179db073458d1c078045aba8847d6
apoloSilva <apolodelima@gmail.com>
2026-03-27 08:53:24 -0300

    tarefa 1 completa

--- contador_netlist.tcl
+++ contador_netlist.tcl
@@ -24,16 +24,43 @@ set palavraFFD "flipflop_D"
 ## Contando quantas vezes cada  palavra aparece no arquivo .v, uma linha por vez
 # incompleto, pois string first retorna apenas a ocorrência da palavra na linha
 foreach line $data {
-
-	set idx [string first $palavraAND2 $line]
-
-	if {$idx != -1} {
-		puts "---TEST 1---"
-		puts "$palavraAND2 Found"
-		puts "-----------"
-
+	
+	# contador de "AND2" 
+	set pos 0
+	while {1} { #loop com break para quando nenhuma palavra "AND2" é encontrada na linha
+		set idx [string first $palavraAND2 $line $pos]
+		if {$idx == -1} {
+			break
+		} else {
+			set ctrADN2 [expr {$ctrADN2 + 1}]
+			set pos [expr {$idx + 4}] ;# atualiza o valor de pos para a posição na linha após a palvra
+		}
 	}
-
+	
+	# contador de "XOR2"
+	set pos 0
+	while {1} { #loop com break para quando nenhuma palavra "XOR2" é encontrada na linha
+		set idx [string first $palavraXOR2 $line $pos]
+		if {$idx == -1} {
+			break
+		} else {
+			set ctrXOR2 [expr {$ctrXOR2 +1}]
+			set pos [expr {$idx + 4}] ;# atualiza o valor de pos para a posição após o idex da palavra encontrada
+		}
+	}
+	
+	# contador de "flipflop_D"
+	set pos 0
+	while {1} { #loop com até encontrar que nenhuma palavra "flipflop_D" seja encontrada na linha
+		set idx [string first $palavraFFD $line $pos]
+		if {$idx == -1} {
+			break
+		} else {
+			set ctrFLIPFLOP_D [expr + {$ctrFLIPFLOP_D + 1}]
+			set pos [expr {$idx + 10}] ;# atualiza o valor de pos para a posição após o idex da palavra encontrada
+		}
+	}
+	
 }
 
 puts "$palavraAND2: $ctrADN2 instâncias"
@@ -41,5 +68,3 @@ puts "$palavraXOR2: $ctrXOR2 instâncias"
 puts "$palavraFFD: $ctrFLIPFLOP_D instâncias"
 set total [expr {$ctrADN2 + $ctrXOR2 + $ctrFLIPFLOP_D}]
 puts "TOTAL: $total"
-
-
